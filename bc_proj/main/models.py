aa555c630fc728af547ebc4844eb71ae9a5daac1
Hvesko_Gennady_253502 <pprom5573@gmail.com>
2024-05-08 02:18:36 +0300

    Created models for the main public pages in the "main" application and migrated
    Signed-off-by: Hvesko_Gennady_253502 <pprom5573@gmail.com>

--- bc_proj/main/models.py
+++ bc_proj/main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50)
+    summary = models.CharField('Краткая информация', max_length=250)
+    image = models.ImageField('Картинка', upload_to='main/images')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+class CompanyInfo(models.Model):
+    info = models.TextField('Информация о компании')
+
+    def __str__(self):
+        return "Информация о компании"
+
+    class Meta:
+        verbose_name = 'Информация о компании'
+        verbose_name_plural = 'Информация о компании'
+
+class FAQ(models.Model):
+    question = models.CharField('Вопрос', max_length=250)
+    answer = models.TextField('Ответ')
+    date_added = models.DateTimeField('Дата добавления', auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Часто задаваемый вопрос'
+        verbose_name_plural = 'Часто задаваемые вопросы'
