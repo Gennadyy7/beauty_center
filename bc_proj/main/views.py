b96aff28438ff0fce2d6eb87a849cb733e4d2648
Hvesko_Gennady_253502 <pprom5573@gmail.com>
2024-05-08 23:16:57 +0300

    The remaining HTML pages of the main application have been added
    Signed-off-by: Hvesko_Gennady_253502 <pprom5573@gmail.com>

--- bc_proj/main/views.py
+++ bc_proj/main/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from .models import Articles
+from .models import Articles, CompanyInfo, FAQ
 
 def index(request):
     latest_article = Articles.objects.last()
     return render(request, 'main/index.html', {'latest_article': latest_article})
 
 def about(request):
-    pass
+    company_info = CompanyInfo.objects.last()
+    return render(request, 'main/about.html', {'company_info': company_info})
 
 def news(request):
     articles = Articles.objects.order_by('-id')
     return render(request, 'main/news.html', {'articles': articles})
 
 def faq(request):
-    pass
+    faq_items = FAQ.objects.order_by('date_added')
+    return render(request, 'main/faq.html', {'faq_items': faq_items})
