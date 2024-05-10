15ef1a6bf0ed2c1d9b60f4e2da291cd8fbf42cb1
Hvesko_Gennady_253502 <pprom5573@gmail.com>
2024-05-10 19:44:01 +0300

    Registered the new models
    Signed-off-by: Hvesko_Gennady_253502 <pprom5573@gmail.com>

--- bc_proj/clinic/admin.py
+++ bc_proj/clinic/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Services, ServiceSpecializations, Specializations, ServiceCategories, Clients, Doctors, Orders, Reviews, PromoCodes
 
-# Register your models here.
+admin.site.register(Services)
+admin.site.register(ServiceSpecializations)
+admin.site.register(Specializations)
+admin.site.register(ServiceCategories)
+admin.site.register(Clients)
+admin.site.register(Doctors)
+admin.site.register(Orders)
+admin.site.register(Reviews)
+admin.site.register(PromoCodes)
