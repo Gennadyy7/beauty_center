21e850be94d904e9fcfa94374b50437c55269bdf
Hvesko_Gennady_253502 <pprom5573@gmail.com>
2024-05-13 01:48:14 +0300

    Date and time and corrections
    Signed-off-by: Hvesko_Gennady_253502 <pprom5573@gmail.com>

--- bc_proj/main/views.py
+++ bc_proj/main/views.py
@@ -4,14 +4,23 @@ from urllib.request import urlopen
 
 from django.core.files import File
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from translate import Translator
-
+from urllib.error import HTTPError
 from .models import Articles, CompanyInfo, FAQ
 import requests
+import pytz
 
 def index(request):
     latest_article = Articles.objects.last()
-    return render(request, 'main/index.html', {'latest_article': latest_article})
+    user_timezone = 'Europe/Moscow'
+    tz = pytz.timezone(user_timezone)
+    current_date = timezone.now().astimezone(tz)
+    return render(request, 'main/index.html', {
+        'latest_article': latest_article,
+        'current_date': current_date,
+        'user_timezone': user_timezone,
+    })
 
 def about(request):
     company_info = CompanyInfo.objects.last()
@@ -30,28 +39,29 @@ def add_cosmetology_news(request):
         'https://newsapi.org/v2/everything?q=cosmetology&apiKey=aabb6aa60cd64803ad49bcb76aec6ced')
     data = response.json()
 
-    article_data = data['articles'][random.choice([i for i in range(64) if i != 43])]
-    title = article_data['title']
-    summary = article_data['description']
-    image_url = article_data['urlToImage']
+    articles = data['articles']
+    random.shuffle(articles)
+    for article_data in articles:
+        title = article_data.get('title')
+        summary = article_data.get('description')
+        image_url = article_data.get('urlToImage')
 
-    print('Это заголовок')
-    print(title)
-    print('Это содержание')
-    print(summary)
-    translator = Translator(to_lang="ru")
-    title_ru = translator.translate(title)
-    summary_ru = translator.translate(summary)
-    print('Это заголовок')
-    print(title_ru)
-    print('Это содержание')
-    print(summary_ru)
+        if title and summary and image_url:
+            translator = Translator(to_lang="ru")
+            title_ru = translator.translate(title)
+            summary_ru = translator.translate(summary)
 
-    img_temp = NamedTemporaryFile(delete=True)
-    img_temp.write(urlopen(image_url).read())
-    img_temp.flush()
+            img_temp = NamedTemporaryFile(delete=True)
+            try:
+                img_temp.write(urlopen(image_url).read())
+            except HTTPError as e:
+                print(f"HTTP error occurred: {e}")
+                continue
+            img_temp.flush()
 
-    article = Articles(title=title_ru, summary=summary_ru)
-    article.image.save(f"{title_ru}.jpg", File(img_temp))
-    article.save()
+            article = Articles(title=title_ru, summary=summary_ru)
+            article.image.save(f"{title_ru}.jpg", File(img_temp))
+            article.save()
+            break
+
     return redirect('home')
