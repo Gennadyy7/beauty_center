71e3f682709d9411bd08b5c44fbdb1749d286a8b
Gennady_Hvesko <pprom5573@gmail.com>
2024-09-10 09:32:35 +0300

    banners and registered partners

--- bc_proj/main/admin.py
+++ bc_proj/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Articles, CompanyInfo, FAQ
+from .models import Articles, CompanyInfo, FAQ, Partners
 
 admin.site.register(Articles)
 admin.site.register(CompanyInfo)
 admin.site.register(FAQ)
+admin.site.register(Partners)
